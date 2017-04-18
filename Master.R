c25c940c327c6bbacadd9d2fcbe1e825474e7ef0
Chris <chris.dobbe@hotmail.com>
2017-04-18 11:04:58 -0700

    few small code changes

--- Master.R
+++ Master.R
@@ -30,22 +30,25 @@ library(caret)
 
 # Begin by loading in the dataset using the appropriate method, uncomment chosen method
 
-#data1 <- file.choose()                                                 # Allows for manual selection from folder location
-#data1 <- read.csv("file_name.csv")                                     #read in standard CSV
-#data1 <- read.csv("file_name.tsv", sep = "\t", header = TRUE)          #read in TSV file
-#data1 <- read.delim("file_name.txt", sep = "\t", header = TRUE)        #read in .txt files
-#data1 <- read.xls("file_name.xlsx")                                    #read in Excel file
-#data1 <- XLGetRange(sheet = "sheet1", range = "A1:B21", header = TRUE) # used with Excel file being open
+#data1 <- file.choose() # Allows for manual selection from folder location, paired with below
+  #data <- read_csv(data1)  # 
+#data1 <- read_csv("file_name.csv") #loads in the CSV to a tibble, easier to work with
+#data1 <- read.csv("file_name.csv")
+#data1 <- read.csv("file_name.tsv", sep = "\t", header = TRUE)
+#data1 <- read.xls("file_name.xlsx")
+#data1 <- XLGetRange(sheet = "sheet1", range = "A1:B21", header = TRUE) #only used when Excel workbook is open
+
+#View dataset
+View(data1)
 
 # View summary info on the dataset that was loaded in
-View(data1)
 names(data1)
 head(data1)
 str(data1)
 summary(data1)
 
 #For modeling, you will want to create a training and test data set
-set.seed(1)      # setting the seed will allow you to adjust the random datasset in training, or keep the same data
+set.seed(1)      # setting the seed will allow you to adjust the random dataset in training, or keep the same data by changing seed #
 in_train = createDataPartition(y = data1$var_1, p = 0.75, list = FALSE)    # Puts 75% of observations in the training dataset
 head(in_train)    # row indices of observations in the training dataset
 
