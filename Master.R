5bac67643148c12d2035aed7509f8540719cde55
Chris <chris.dobbe@hotmail.com>
2017-04-19 11:05:31 -0700

    added confidence interval code
    Added code to produce value estimates for a linear model based on upper/lower confidence intervals

--- Master.R
+++ Master.R
@@ -28,7 +28,7 @@ library(GGally)
 library(scales)
 library(caret)
 
-# Begin by loading in the dataset using the appropriate method, uncomment chosen method
+### Begin by loading in the dataset using the appropriate method, uncomment chosen method
 
 #data1 <- file.choose() # Allows for manual selection from folder location, paired with below
   #data <- read_csv(data1)  # 
@@ -41,13 +41,13 @@ library(caret)
 #View dataset
 View(data1)
 
-# View summary info on the dataset that was loaded in
+### View summary info on the dataset that was loaded in
 names(data1)
 head(data1)
 str(data1)
 summary(data1)
 
-#For modeling, you will want to create a training and test data set
+### For modeling, you will want to create a training and test data set
 set.seed(1)      # setting the seed will allow you to adjust the random dataset in training, or keep the same data by changing seed #
 in_train = createDataPartition(y = data1$var_1, p = 0.75, list = FALSE)    # Puts 75% of observations in the training dataset
 head(in_train)    # row indices of observations in the training dataset
@@ -58,7 +58,7 @@ test = data1[-in_train, ]
 dim(train)
 dim(test)
 
-#create the linear regression model, choosing 2 variables to examine
+### create the linear regression model, choosing 2 variables to examine
 
 model <- lm(var_1 ~ var_2, data = data1)    #produces the linear model between the two var.
 attributes(model)                           # returns list of all things inside the model, handy for calling specific elements later
@@ -76,13 +76,13 @@ ggplot(data1, aes(x = var_1, y = var_2)) +  # creates scatter plot
 #     of 1 in the 2nd variable, the 1st variables goes up by the 2nd var. estimate
 #   we want to see a large t value >= 2.5, and a tiny p value to indicate correlation
 
-# This plot includes a 3rd variable in the plot, performing multivariate regression
+### This plot includes a 3rd variable in the plot, performing multivariate regression
 # produces same result as first model, but includes additional variable correlated with var. 1
 
 model2 <- lm(var_1 ~ var_2 + var_3, data = data1)
 summary(model2)             # Output will show how each var_2 and var_3 correlate to var_1, using same metrics
 
-#below chart plots two variables, and adds size and color of point for a 3rd variable
+### below chart plots two variables, and adds size and color of point for a 3rd variable
 
 ggplot(bike, aes(y = var_4, x = var_5)) +
   geom_smooth(method = 'lm', se = FALSE, color = 'black') + 
@@ -92,10 +92,20 @@ ggplot(bike, aes(y = var_4, x = var_5)) +
   scale_size(range = c(0, 15)) + 
   theme_minimal()
 
-# explore the residuals further to understand their distribution and trend of linear model
+### explore the residuals further to understand their distribution and trend of linear model
 residuals(model)  # lists the distance from the mean for each observation in the model
 summary(residuals(model))
 plot(fitted(model), residuals(model))
 abline(h = 0)  #plots the residual values around the mean line of 0
 qqnorm(residuals(model))
 qqline(residuals(model)) # additional residual plot 
+
+### Confidence Intervals for a linear model to predict values
+
+plot(data1$mpg, data1$hp)
+ci <- confint(model, level=0.95) #creates the confidence interval, using a specified sign. level
+ci                               
+lower_est = ci[1] * 9 + ci[2]  #creates lowest estimate for the x/y value based on where the *x is
+lower_est                       #estimating x or y depends on where the *x goes, on 1sr or 2nd ci[]
+upper_est = ci[3] * 9 + ci[4]  #creates upper estimate for x/y value 
+upper_est
